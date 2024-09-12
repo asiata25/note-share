d72c753cd7005db838d4c598286a6fb3e298c2eb
Mohamad Lutfi Khoirudin <asiatakh25@gmail.com>
2024-09-12 23:09:21 +0700

    build: add wire ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <wireui:scripts />
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
